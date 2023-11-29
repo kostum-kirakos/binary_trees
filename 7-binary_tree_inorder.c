b43d321e35e9e8b30ef0d4a81f1b041c13b688d4
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-11-29 09:58:41 -0800

    task 7 fixed

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -6,7 +6,7 @@
  * @func: pointer to a function to call for each node
  */
 
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
     if (tree == NULL || func == NULL)
         return;
