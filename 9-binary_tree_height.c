48719923a56053fba1011a5e5d6036996a363e8d
Rida Boutskaouin <rida.boutskaouin@edu.uiz.ac.ma>
2023-11-29 20:13:52 +0100

    fix betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,16 +9,16 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = binary_tree_height(tree->left) + 1;
-    right_height = binary_tree_height(tree->right) + 1;
+	left_height = binary_tree_height(tree->left) + 1;
+	right_height = binary_tree_height(tree->right) + 1;
 
-    if (left_height > right_height)
-        return (left_height);
-    else
-        return (right_height);
+	if (left_height > right_height)
+		return (left_height);
+	else
+		return (right_height);
 }
