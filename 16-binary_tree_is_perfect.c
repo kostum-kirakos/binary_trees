181d8a84fc4e030fb70cf5578a640dce5f519d97
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-11-30 05:23:14 -0800

    function checks for perfect binary tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -2,28 +2,28 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-  /**
-   * if (!tree)
-   * {return (0); }
-   *
-   * if (tree->left && tree->right)
-   * {
-   *	return (binary_tree_is_full(tree->left) &&
-   * binary_tree_is_full(tree->right));
-   */
+	/**
+	 * if (!tree)
+	 * {return (0); }
+	 *
+	 * if (tree->left && tree->right)
+	 * {
+	 *	return (binary_tree_is_full(tree->left) &&
+	 * binary_tree_is_full(tree->right));
+	 */
 
-  if (tree == NULL)
-  {return (0); }
+	if (tree == NULL)
+	{return (0); }
 
-  if (!tree->right && !tree->left)
-  {return (1); }
+	if (!tree->right && !tree->left)
+	{return (1); }
 
-  if (tree->left && tree->right)
-  {
-    return (binary_tree_is_full(tree->left) &&
-     binary_tree_is_full(tree->right));
-  }
-  return (0);
+	if (tree->left && tree->right)
+	{
+		return (binary_tree_is_full(tree->left) &&
+				binary_tree_is_full(tree->right));
+	}
+	return (0);
 }
 
 /**
@@ -34,23 +34,23 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-  size_t count_1 = 0, count_2 = 0;
+	size_t count_1 = 0, count_2 = 0;
 
-  if (tree != NULL)
-  {
-    if (tree == NULL)
-    {return (0); }
+	if (tree != NULL)
+	{
+		if (tree == NULL)
+		{return (0); }
 
-    count_1 = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    count_2 = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		count_1 = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		count_2 = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-    if (count_1 > count_2)
-    {return (count_1); }
-    else
-    {return (count_2); }
+		if (count_1 > count_2)
+		{return (count_1); }
+		else
+		{return (count_2); }
 
-  }
-  return (0);
+	}
+	return (0);
 }
 
 /**
@@ -61,20 +61,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-  int count_1 = 0, count_2 = 0;
+	int count_1 = 0, count_2 = 0;
 
-  if (tree != NULL)
-  {
-    if (tree == NULL)
-    {return (0); }
+	if (tree != NULL)
+	{
+		if (tree == NULL)
+		{return (0); }
 
-    count_1 = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    count_2 = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		count_1 = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		count_2 = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-    return (count_1 - count_2);
+		return (count_1 - count_2);
 
-  }
-  return (0);
+	}
+	return (0);
 }
 
 /**
@@ -85,9 +85,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-  if (tree == NULL)
-    return (0);
-  if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
-    return (1);
-  return (0);
+	if (tree == NULL)
+		return (0);
+	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
+		return (1);
+	return (0);
 }
