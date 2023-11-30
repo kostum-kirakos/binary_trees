ce6b9c8c1a0bee4a6e196b909233bc1264ace728
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-11-30 06:06:32 -0800

    function checks for siblings of a node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,9 +11,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
-	if (node->parent->right != NULL && node->parent->left != NULL)
-		if (node->parent->right == node)
-			return (node->parent->left);
 	if (node->parent->left == node)
 		return (node->parent->right);
+	return (node->parent->left);
 }
