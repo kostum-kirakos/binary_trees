fdddf598495aadb37829a1d3ba6e36a7b649abd9
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-11-30 06:15:08 -0800

    function checks for uncle of a node

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - function that finds the sibling of a node
+ * @node: pointer to the node to find sibling
+ *
+ * Return: If node NULL or parents NULL return NULL and No sibling return NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+  if (node == NULL || node->parent == NULL)
+    return (NULL);
+  if (node->parent->left == node)
+    return (node->parent->right);
+  return (node->parent->left);
+}
+
+/**
+ * binary_tree_uncle - function that finds the sibling of a node
+ * @node: pointer to the node to find uncle
+ *
+ * Return: If node NULL  and No uncle return NULL
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+  if (node == NULL)
+    return  (NULL);
+  if (node->parent != NULL)
+    return (binary_tree_sibling(node->parent));
+}
