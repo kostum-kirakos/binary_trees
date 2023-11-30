d6b5744bd8d79c079d839f85dbbc4b319b929de7
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-11-30 06:01:56 -0800

    function checks for siblings of a node

--- binary_trees.h
+++ binary_trees.h
@@ -30,6 +30,7 @@ typedef struct binary_tree_s
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
@@ -44,5 +45,4 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
-binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 #endif /* BINARY_TREES_H */
