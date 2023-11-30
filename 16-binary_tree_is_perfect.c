1c22181214c7e566ee6ae91b8a37898c4deb81d1
kostum-kirakos <lkoustim.mohamed@yahoo.com>
2023-11-30 10:49:42 -0800

    fix task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -82,11 +82,23 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+
+	int balance;
+
 	if (tree == NULL)
 		return (0);
 
-	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
-		return (binary_tree_is_full(tree));
+	balance = binary_tree_balance(tree);
+	if (balance != 0)
+		return (0);
 
-	return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left && tree->right)
+		return (binary_tree_is_perfect(tree->left) && 
+				binary_tree_is_perfect(tree->right));
+
+	return (1);
 }
+
